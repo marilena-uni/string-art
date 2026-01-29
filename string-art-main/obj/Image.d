c03c772fa790ab1291aa84fdace028815bbb80fc
marilena-uni <marilena.r03@outlook.it>
2026-01-29 10:30:06 +0100

    Add files via upload

---
+++ string-art-main/obj/Image.d
@@ -0,0 +1,3 @@
+obj/Image.o: src/Image.cpp src/Image.h src/Point2D.h
+src/Image.h:
+src/Point2D.h:
---
+++ string-art-main/obj/StringArtist.d
@@ -0,0 +1,8 @@
+obj/StringArtist.o: src/StringArtist.cpp src/StringArtist.h \
+ src/StringArtImage.h src/Image.h src/Point2D.h \
+ src/BresenhamLineIterator.h
+src/StringArtist.h:
+src/StringArtImage.h:
+src/Image.h:
+src/Point2D.h:
+src/BresenhamLineIterator.h:
