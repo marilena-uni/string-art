3689063e355f3977a6767ebddf416b9af51ff74d
merilaaouaj <meriem03.laaouaj@gmail.com>
2026-02-07 16:57:49 +0100

    Delete string-art-main directory

--- string-art-main/obj/Image.d
+++
@@ -1,3 +0,0 @@
-obj/Image.o: src/Image.cpp src/Image.h src/Point2D.h
-src/Image.h:
-src/Point2D.h:
--- string-art-main/obj/StringArtist.d
+++
@@ -1,8 +0,0 @@
-obj/StringArtist.o: src/StringArtist.cpp src/StringArtist.h \
- src/StringArtImage.h src/Image.h src/Point2D.h \
- src/BresenhamLineIterator.h
-src/StringArtist.h:
-src/StringArtImage.h:
-src/Image.h:
-src/Point2D.h:
-src/BresenhamLineIterator.h:
